1676fa1763553a1007a9a59c45427a9281fdc4c7
Bastian Meier <post@bastianmeier.de>
2014-03-16 14:13:29 +0100

    grunt-gh-pages added

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function (grunt) {
         },
         'gh-pages': {
             options: {
-                base: ['demo', 'node_modules']
+                base: 'demo'
             },
             src: ['**']
         }
